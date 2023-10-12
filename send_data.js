250624ff9610dd825fa0ba01edd76e3fcc598ad7
Kurbis12 <75926481+Kurbis12@users.noreply.github.com>
2023-10-12 16:40:59 +0300

    Update send_data.js

--- send_data.js
+++ send_data.js
@@ -1,5 +1,5 @@
 // Replace 'your_dash_server_url' with the actual URL of your Dash app server
-const dashServerUrl = 'http://192.168.200.223/receive_scanned_data';
+const dashServerUrl = 'your url/receive_scanned_data';
 
 const scannedData = 'scanned_result';  // Replace with your actual scanned data
 
