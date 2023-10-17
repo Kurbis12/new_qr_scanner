6f4e104675af4394a51f01471e47cf638547f48f
Kurbis12 <75926481+Kurbis12@users.noreply.github.com>
2023-10-17 18:55:53 +0300

    Add files via upload

--- send_data.js
+++ send_data.js
@@ -1,7 +1,7 @@
-// Replace 'your_dash_server_url' with the actual URL of your Dash app server
-const dashServerUrl = 'your url/receive_scanned_data';
+// Replace 'your_dash_server_url' URL of your Dash app server
+const dashServerUrl = 'http://192.168.200.223/receive_scanned_data';
 
-const scannedData = 'scanned_result';  // Replace with your actual scanned data
+const scannedData = 'scanned_result'; // Replace with your actual scanned data
 
 // Create JSON data to send
 const jsonData = {
@@ -10,18 +10,18 @@ const jsonData = {
 
 // Send a POST request to the Dash app server with CORS headers
 fetch(dashServerUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*' // Add this header to enable CORS
-    },
-    body: JSON.stringify(jsonData),
-})
-.then(response => response.json())
-.then(data => {
-    // Handle the response from the server if needed
-    console.log(data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*' // Add this header to enable CORS (Has TypeError: Failed to fetch)
+        },
+        body: JSON.stringify(jsonData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the response from the server if needed
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
